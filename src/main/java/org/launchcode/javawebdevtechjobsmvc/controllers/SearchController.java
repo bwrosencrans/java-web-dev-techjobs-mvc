c74dcf13b7dceb5691fc9d353f865848a3bbb09c
Brad Rosencrans <brad.rosencrans@outlook.com>
2020-01-08 01:50:48 -0600

    finished

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,33 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @RequestMapping(value = "results")
+    public String resultsNoPost(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @RequestMapping(value = "results", method = RequestMethod.POST)
+    public String results(Model model, @RequestBody String request) {
+        model.addAttribute("columns", columnChoices);
+
+        ArrayList<Job> jobs = null;
+
+        String[] keyValues = request.split("&");
+        if(keyValues.length == 2) {
+            String[] searchType = keyValues[0].split("=");
+            String[] searchTerm = keyValues[1].split("=");
+
+            jobs = JobData.findByColumnAndValue(searchType[1], searchTerm[1]);
+        }
+        else if(keyValues.length == 1) {
+            String[] searchTerm = keyValues[0].split("=");
+            jobs = JobData.findByValue(searchTerm[1]);
+        }
+
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
 
 }
